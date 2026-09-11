74c17f5d24a5677b47dc4c793001ee53d47beaed
Zuzuitu <178236894+Zuzuitu@users.noreply.github.com>
2026-09-11 20:25:20 +0200

    fix: build save popup with explicit ReDOM elements

--- web/src/ui/saveSubsPopup.jsx
+++ web/src/ui/saveSubsPopup.jsx
@@ -11,28 +11,29 @@ export default class SaveSubtitlesPopup extends OverlayItem {
     super(i18n`Select format`, true);
     this.props = props;
 
-    <div this='content'>
-      <dl>
-        <dt>SubRip:</dt>
-        <dd><a this='srtButton' /></dd>
-        <dt>Advanced Substation:</dt>
-        <dd><a this='assButton' /></dd>
-        <dt>TMP:</dt>
-        <dd><a this='tmpButton' /></dd>
-      </dl>
-      {props.lang && this.renderAppendLangCheckbox()}
-    </div>;
+    this.srtButton = el('a');
+    this.assButton = el('a');
+    this.tmpButton = el('a');
 
-    this.update();
-  }
-
-  renderAppendLangCheckbox() {
-    return(
-      <label>
-        <input type='checkbox' this='appendLang' onclick={this.update.bind(this)} />
-        {i18n`append language code`}
-      </label>
+    const formatList = el(
+      'dl',
+      el('dt', 'SubRip:'),
+      el('dd', this.srtButton),
+      el('dt', 'Advanced Substation:'),
+      el('dd', this.assButton),
+      el('dt', 'TMP:'),
+      el('dd', this.tmpButton),
     );
+
+    const children = [formatList];
+    if (props.lang) {
+      this.appendLang = el('input', {type: 'checkbox'});
+      this.appendLang.onclick = this.update.bind(this);
+      children.push(el('label', this.appendLang, i18n`append language code`));
+    }
+
+    this.content = el('div', children);
+    this.update();
   }
 
   update() {
@@ -56,23 +57,28 @@ export default class SaveSubtitlesPopup extends OverlayItem {
   }
 
   save(fmt, name) {
+    let url;
+    let a;
     try {
       const subs = Synchronizer.instance.getSynchronizedSubtitles(fmt);
       if (!subs) {
         throw new Error(i18n`Subtitles not ready!`);
       }
       const file = new Blob(subs, {type: 'text/plain'});
-      const url = URL.createObjectURL(file);
-      const a = <a download={name} href={url} hidden />;
+      url = URL.createObjectURL(file);
+      a = el('a', {download: name, href: url, hidden: true});
       mount(document.body, a);
       a.click();
-      setTimeout(() => {
-        unmount(document.body, a);
-        window.URL.revokeObjectURL(url);
-      }, 0);
     } catch (e) {
       logger.warn('save failed:', e);
       Overlay.showErrorPopup(i18n`Couldn't save subtitles`, e);
+    } finally {
+      if (a && url) {
+        setTimeout(() => {
+          unmount(document.body, a);
+          window.URL.revokeObjectURL(url);
+        }, 0);
+      }
     }
   }
 }
