70110480e90cd85f357aef88cc46d3fc68b6ab6b
Zuzuitu <178236894+Zuzuitu@users.noreply.github.com>
2026-09-12 18:07:23 +0200

    Connect Romanian PCM to isolated Whisper SIMD

---
+++ web/src/extractor/romanian-whisper.js
@@ -0,0 +1,113 @@
+import settings from '../settings.js';
+import version from '../../version.json';
+import Logger from '../logger.js';
+
+const logger = Logger.logger.get('[RomanianWhisper]');
+let modulePromise;
+
+function locateFile(path) {
+  let url = `${settings.url}scripts/${path}`;
+  if (version.hash) {
+    url += `?${version.hash}`;
+  }
+  return url;
+}
+
+async function getModule() {
+  if (!modulePromise) {
+    const factory = require('../../scripts/whisper.js');
+    modulePromise = factory({
+      locateFile,
+      print: logger.log.bind(logger),
+      printErr: logger.warn.bind(logger),
+    }).then(instance => {
+      const info = instance.whisperSystemInfo();
+      if (!info || !info.includes('WASM_SIMD = 1')) {
+        throw new Error(
+          'Romanian speech recognition requires WebAssembly SIMD support.'
+        );
+      }
+      logger.log('ready', info);
+      return instance;
+    });
+  }
+  return modulePromise;
+}
+
+export default class RomanianSpeechRecognition {
+  static async create(modelBytes) {
+    const module = await getModule();
+    const recognition = new RomanianSpeechRecognition(module);
+    recognition.recognizer.loadModel(modelBytes);
+    return recognition;
+  }
+
+  constructor(module) {
+    this.module = module;
+    this.recognizer = new module.RomanianWhisperRecognition();
+    this.listeners = [];
+    this.recognizer.setWordsCallback(word => {
+      for (const listener of this.listeners) {
+        listener(word);
+      }
+    });
+  }
+
+  setLanguage(language) {
+    this.recognizer.setLanguage(language);
+  }
+
+  setMinWordProb(value) {
+    this.recognizer.setMinWordProb(value);
+  }
+
+  setMinWordLen(value) {
+    this.recognizer.setMinWordLen(value);
+  }
+
+  addWordsListener(listener) {
+    this.listeners.push(listener);
+  }
+
+  connectTranslator(translator) {
+    this.addWordsListener(word => {
+      translator.pushWordValues(
+        word.text,
+        word.time,
+        word.duration || 0,
+        word.score == null ? 1 : word.score
+      );
+    });
+  }
+
+  connectNgramSplitter(splitter) {
+    this.addWordsListener(word => {
+      splitter.pushWordValues(
+        word.text,
+        word.time,
+        word.duration || 0,
+        word.score == null ? 1 : word.score
+      );
+    });
+  }
+
+  connectPcmSink(sink) {
+    sink.setFeedCallback((samples, startTime) => {
+      this.recognizer.feed(samples, startTime);
+    });
+    sink.setFlushCallback(() => {
+      this.recognizer.flush();
+    });
+    sink.setDiscontinuityCallback(() => {
+      this.recognizer.discontinuity();
+    });
+  }
+
+  delete() {
+    this.listeners = [];
+    if (this.recognizer) {
+      this.recognizer.delete();
+      this.recognizer = null;
+    }
+  }
+}
